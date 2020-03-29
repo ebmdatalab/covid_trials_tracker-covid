81a8eafbf4cdb475b9eb96c4d4a95c9a88c3b8cc
NickCEBM <nicholas.devito@phc.ox.ac.uk>
2020-03-29 18:27:52 +0100

    Notebook and website updates
    Cleaned up notebook directory, data sources, and notebook code
    
    Updated data to 25 March 2020
    
    Added intervention type and intervention data
    
    Added new chart for intervention types

--- notebooks/diffable_python/ictrp_data_handling.py
+++ notebooks/diffable_python/ictrp_data_handling.py
@@ -24,22 +24,23 @@ import numpy as np
 from datetime import date
 import unicodedata
 
-#POINT THIS TO THE UPDATED XML
+#POINT THIS TO THE UPDATED XML - deprecated as ICTRP is down.
+
 #with open('ICTRP-Results_18Mar2020.xml', 'rb') as f:
 #    xml = xmltodict.parse(f, dict_constructor=dict)
 
 #df = pd.DataFrame(xml['Trials_downloaded_from_ICTRP']['Trial'])
 
+#This now takes the CSV posted by the ICTRP as an input from here: https://www.who.int/ictrp/en/
 
-df = pd.read_excel('covid-19-trials_25Mar2020.xlsx', dtype={'Phase': str})
+df = pd.read_excel('this_weeks_data/covid-19-trials_25Mar2020.xlsx', dtype={'Phase': str})
 
 #UPDATE THESE WITH EACH RUN
 prior_extract_date = date(2020,3,18)
 this_extract_date = date(2020,3,25)
 
 # +
-#For future:
-#Can try and parse the 'Target_size' variable, difficult for the Chinese registry
+#Taking only what we need right now
 
 cols = ['TrialID', 'Source Register', 'Date registration', 'Date enrollement', 'Primary sponsor', 
         'Recruitment Status', 'Phase', 'Study type', 'Countries', 'Public title', 'Intervention',
@@ -47,63 +48,31 @@ cols = ['TrialID', 'Source Register', 'Date registration', 'Date enrollement', '
 
 df_cond = df[cols].reset_index(drop=True)
 
+#renaming columns to match old format so I don't have to change them throughout
 df_cond.columns = ['TrialID', 'Source_Register', 'Date_registration', 'Date_enrollement', 
                    'Primary_sponsor', 'Recruitment_Status', 'Phase', 'Study_type', 'Countries', 
                    'Public_title', 'Intervention', 'web_address', 'results_url_link', 
                    'Last_Refreshed_on']
 
 print(f'Search on ICTRP reveals {len(df_cond)} trials as of {this_extract_date}')
-
-# +
-#POINT THIS TO LAST WEEK'S DATA
-last_weeks_trials = pd.read_csv('trial_data_18_mar.csv')
-
-df_cond = df_cond.merge(last_weeks_trials[['trialid', 'first_seen']], left_on = 'TrialID', right_on = 'trialid', 
-                        how='left').drop('trialid', axis=1)
 # -
 
-#For next time
+#POINT THIS TO LAST WEEK'S DATA
+last_weeks_trials = pd.read_csv('last_weeks_data/trial_data_18_mar.csv')
+
+# +
+#Joining in the 'first_seen' field
+df_cond = df_cond.merge(last_weeks_trials[['trialid', 'first_seen']], left_on = 'TrialID', right_on = 'trialid', 
+                        how='left').drop('trialid', axis=1)
+
+#Adding the `first_seen` field to new trials
 df_cond['first_seen'] = pd.to_datetime(df_cond['first_seen'].fillna(this_extract_date))
+# -
 
 #Check for which registries we are dealing with:
 df_cond.Source_Register.value_counts()
 
-# This is the first area where we may need manual intervention on updates. As more registries start to appear, there may be dates in new formats that we need to address. Just running a date parse over the column, even with just two registries, was already producing wonky dates so I had to split it by registry. Check this based on the registries above and adjust.
-#
-# It looks like this is now standardized well enough in the ICTRP excel file that pandas can parse right on loading
-
-# #last refreshed date parse
-# df_cond['Last_Refreshed_on'] = pd.to_datetime(df_cond['Last_Refreshed_on'])
-#
-# #cleaning up registration dates
-#
-# date_parsing_reg = df_cond[['TrialID', 'Date_registration']].reset_index(drop=True)
-#
-# ncts = date_parsing_reg[date_parsing_reg['TrialID'].str.contains('NCT')].reset_index(drop=True)
-# ncts['parsed_date'] = pd.to_datetime(ncts['Date_registration'], format='%d/%m/%Y')
-#
-# chictr = date_parsing_reg[date_parsing_reg['TrialID'].str.contains('Chi')].reset_index(drop=True)
-# chictr['parsed_date'] = pd.to_datetime(chictr['Date_registration'], format='%Y-%m-%d')
-#
-# nct_merged_reg = df_cond.merge(ncts[['TrialID','parsed_date']], on='TrialID', how='left')
-# chi_merged_reg = nct_merged_reg.merge(chictr[['TrialID','parsed_date']], on='TrialID', how='left')
-#
-# df_cond['Date_registration'] = chi_merged_reg['parsed_date_x'].fillna(chi_merged_reg['parsed_date_y'])
-#
-# #cleaning up start dates
-#
-# date_parsing_enr = df_cond[['TrialID', 'Date_enrollement']].reset_index(drop=True)
-#
-# ncts = date_parsing_enr[date_parsing_enr['TrialID'].str.contains('NCT')].reset_index(drop=True)
-# ncts['parsed_date'] = pd.to_datetime(ncts['Date_enrollement'])
-#
-# chictr = date_parsing_enr[date_parsing_enr['TrialID'].str.contains('Chi')].reset_index(drop=True)
-# chictr['parsed_date'] = pd.to_datetime(chictr['Date_enrollement'], format='%Y-%m-%d')
-#
-# nct_merged_enr = df_cond.merge(ncts[['TrialID','parsed_date']], on='TrialID', how='left')
-# chi_merged_enr = nct_merged_enr.merge(chictr[['TrialID','parsed_date']], on='TrialID', how='left')
-#
-# df_cond['Date_enrollement'] = chi_merged_enr['parsed_date_x'].fillna(chi_merged_enr['parsed_date_y'])
+# When working with data straight from XML we need to do some tedious tidying up of dates because of different formats. They do not parse correctly by default in Pandas. They are standardized, however, in the ICTRP spreadsheet so I have removed this code for now. It is archived in old commits to the GitHub repo for future refrence if needed.
 
 # +
 #lets get rid of trials from before 2020 for now
@@ -117,46 +86,86 @@ df_cond_rec = df_cond[df_cond['Date_registration'] >= pd.Timestamp(2020,1,1)].re
 print(f'{len(df_cond_rec)} trials remain')
 # -
 
-# Point 2 for manual intervention. As more registries add trials, we will have to contend with a wider vocabulary/identification methods for trials that are cancelled/withdrawn.
+# This code removes trials that indicate that they never started. This is done on the Chinese registry through specific language in the Title. Trials from ClinicalTrials.gov are indicated by the `Withdrawn` trial status. 
 #
-# No further conflicts as of 25 Mar 2020
+# This will be expanded moving forward to account for the unique terminology used by other registries as necessary moving forward.
 
 # +
 #Removing cancelled/withdrawn trials for what registries we have to date
 
 cancelled_trials = len(df_cond_rec[(df_cond_rec['Public_title'].str.contains('Cancelled')) | 
-                         (df_cond_rec['Recruitment_Status'] == "Withdrawn")])
+                                   (df_cond_rec['Public_title'].str.contains('Retracted due to')) | 
+                                   (df_cond_rec['Recruitment_Status'] == "Withdrawn")])
 
 print(f'Excluded {cancelled_trials} cancelled trials with no enrollment')
 
-df_cond_nc = df_cond_rec[~(df_cond_rec['Public_title'].str.contains('Cancelled')) & 
-                         ~(df_cond_rec['Recruitment_Status'] == "Withdrawn")].reset_index(drop=True)
+#Now lets get rid of trials we know don't belong from manual review, reason catalogued below
+#NCT04226157 Non-COVID trial delayed because of COVID and put this in the title
+#NCT04246242 This trial registration doesn't exist anymore
+
+exclude = ['NCT04226157', 'NCT04246242']
+
+print(f'Excluded {len(exclude)} non-COVID trials screened through manual review')
+
+df_cond_nc = df_cond_rec[~((df_cond_rec['Public_title'].str.contains('Cancelled')) | 
+                           (df_cond_rec['Public_title'].str.contains('Retracted due to'))) & 
+                         ~(df_cond_rec['Recruitment_Status'] == "Withdrawn") &
+                         ~(df_cond_rec['TrialID'].isin(exclude))].reset_index(drop=True)
 
 print(f'{len(df_cond_nc)} trials remain')
+
+
 # -
 
-# Point three for manual intervention. All this normalisation and data cleaning will have to be expanded each update as more trials get added and more registries start to add trials with their own idiosyncratic data categories. 
+# Now we just need to take a quick look at trials that came and went since the last update. We can add in any additional trials that we know about that are not accounted for in the ICTRP database.
 
 # +
-#Now lets get rid of ones we know don't belong from manual review
+#Function for checking changes
 
-exclude = ['NCT04226157']
-
-df_cond_nc = df_cond_nc[~df_cond_nc['TrialID'].isin(exclude)].reset_index(drop=True)
+def trial_diffs(new=True):
+    df = df_cond_nc.merge(last_weeks_trials['trialid'], left_on = 'TrialID', right_on = 'trialid', how='outer', indicator=True)
+    if new:
+        new = df[(df['_merge'] == 'left_only')]
+        return new['TrialID'].tolist()
+    else:
+        dropped = df[(df['_merge'] == 'right_only')]
+        return dropped['trialid'].tolist()
 
 
 # +
+print(f'There are {len(trial_diffs(new=True))} new trials')
+
+print(f'The following trials were removed since the last time and were manually checked:')
+print(trial_diffs(new=False))
+
+# +
+additions = pd.read_excel('manual_data.xlsx', sheet_name = 'additional_trials')
+
+print(f'An additional {len(additions)} known trials were not in the ICTRP data')
+
+print(set(additions['TrialID'].tolist()).difference(df_cond_nc['TrialID'].tolist()))
+
+df_cond_nc = df_cond_nc.append(additions)
+
+print(f'The final dataset is {len(df_cond_nc)} trials')
+
+
+# -
+
+# Normalisation and data cleaning of all fields. This will be expanded each update as more trials get added and more registries start to add trials with their own idiosyncratic data categories. 
+
+# +
+#A small function to help quickly check the contents of various columns
+
 def check_fields(field):
     return df_cond_nc[field].unique()
 
 #Check fields for new unique values that require normalisation
 check_fields('Study_type')
-# -
-
-check_fields('Countries')
 
 # +
-#More data cleaning
+#Data cleaning various fields. 
+#One important thing we have to do is make sure there are no nulls or else the data won't properly load onto the website
 
 #Can't have nulls in the results field. Might need to move this later on when we start populating results
 df_cond_nc['results_url_link'] = df_cond_nc['results_url_link'].fillna('No Results')
@@ -215,6 +224,8 @@ for c in country_values:
     country_list = []
     if isinstance(c, float):
         country_list.append('No Sponsor Name Given')
+    elif c == 'No Sponsor Name Given':
+        country_list.append('No Sponsor Name Given')
     elif c in china_corr:
         country_list.append('China')
     elif c == 'Iran (Islamic Republic of)':
@@ -247,7 +258,7 @@ df_cond_nc['Countries'] = new_list
 #Run this cell, updating the spon_norm csv you are loading after manual adjusting
 #until you get the 'All sponsor names normalized' to print
 
-spon_norm = pd.read_csv('norm_schedule_25Mar2020.csv')
+spon_norm = pd.read_excel('manual_data.xlsx', sheet_name = 'sponsor')
 
 df_cond_norm = df_cond_nc.merge(spon_norm, left_on = 'Primary_sponsor', right_on ='unique_spon_names', how='left')
 df_cond_norm = df_cond_norm.drop('unique_spon_names', axis=1)
@@ -262,55 +273,26 @@ else:
     print('All sponsor names normalized')
 
 # +
-#interim dataset
-
-col_names = []
-
-for col in list(df_cond_norm.columns):
-    col_names.append(col.lower())
-    
-df_cond_norm.columns = col_names
-
-reorder = ['trialid', 'source_register', 'date_registration', 'date_enrollement', 'normed_spon_names', 
-           'recruitment_status', 'phase', 'study_type', 'countries', 'public_title',
-           'web_address', 'results_url_link', 'last_refreshed_on', 'first_seen']
-
-df_cond_norm[reorder].to_csv('trial_data_25_mar.csv')
-# -
-
-mar18 = pd.read_csv('trial_data_18_mar.csv')
-mar18['results_url_link'] = mar18['results_url_link'].fillna('No Results')
-mar18['phase'] = mar18['phase'].fillna('Not Applicable')
-mar18['recruitment_status'] = mar18['recruitment_status'].fillna('No Status Given')
-
-import json
-with open("trials_18mar.json", "w") as f:
-    json.dump({"data": mar18.values.tolist()}, f, indent=2)
-
-# +
 #Integrating intervention type data
 #Once again, run to bring in the old int-type data, islolate the new ones, update, and rerun until
 #producing the all-clear message
 
-int_type = pd.read_csv('int_type_data_8mar2020.csv')
-df_cond_int = df_cond_norm.merge(int_type, left_on = 'TrialID', right_on = 'trial_id', how='left')
+int_type = pd.read_excel('manual_data.xlsx', sheet_name = 'intervention')
+df_cond_int = df_cond_norm.merge(int_type[['trial_id', 'intervention_type', 'intervention']], left_on = 'TrialID', right_on = 'trial_id', how='left')
 df_cond_int = df_cond_int.drop('trial_id', axis=1)
 
-new_int_trials = df_cond_int[df_cond_int['intervention_type'].isna()]
+new_int_trials = df_cond_int[(df_cond_int['intervention_type'].isna()) | (df_cond_int['intervention'].isna())]
 
 if len(new_int_trials) > 0:
-    new_int_trials[['TrialID', 'Public_title', 'Intervention', 'intervention_type']].to_csv('int_to_assess.csv')
+    new_int_trials[['TrialID', 'Public_title', 'Intervention', 'intervention_type', 'intervention']].to_csv('int_to_assess.csv')
     print('Update the intervention type assessments and rerun')
 else:
     print('All intervention types matched')
+    df_cond_int = df_cond_int.drop('Intervention', axis=1).reset_index(drop=True)
 
 # +
-#take a quick glance at old trials that updated
+#Final organising
 
-df_cond_int[(df_cond_int['Last_Refreshed_on'] > pd.Timestamp(prior_extract_date)) & 
-            (df_cond_int['first_seen'] != this_extract_date)]
-
-# +
 col_names = []
 
 for col in list(df_cond_int.columns):
@@ -319,32 +301,38 @@ for col in list(df_cond_int.columns):
 df_cond_int.columns = col_names
 
 reorder = ['trialid', 'source_register', 'date_registration', 'date_enrollement', 'normed_spon_names', 
-           'recruitment_status', 'phase', 'study_type', 'countries', 'public_title', 'intervention_type', 
+           'recruitment_status', 'phase', 'study_type', 'countries', 'public_title', 'intervention_type', 'intervention',
            'web_address', 'results_url_link', 'last_refreshed_on', 'first_seen']
 
 df_final = df_cond_int[reorder].reset_index(drop=True)
-
-# +
-#df_final.to_csv(f'trial_list_{this_extract_date}.csv')
 # -
 
+#Checking for any null values
+df_final[df_final.isna().any(axis=1)]
+
+#Quick look at the data
 df_final.head()
+
+#Export final dataset
+df_final.to_csv(f'processed_data_sets/trial_list_{this_extract_date}.csv')
+
+#Export json for website
+import json
+with open("website_data/trials_latest.json", "w") as f:
+    json.dump({"data": df_final.values.tolist()}, f, indent=2, default=str)
 
 
 
 # # Overall Trend in Registered Trials Graph
 
 # +
-#just_reg = df_final[['trialid', 'date_registration']].reset_index(drop=True)
-just_reg = mar18[['trialid', 'date_registration']].reset_index(drop=True)
-just_reg['date_registration'] = pd.to_datetime(just_reg['date_registration'], format='%d/%m/%Y')
+just_reg = df_final[['trialid', 'date_registration']].reset_index(drop=True)
+#just_reg = mar18[['trialid', 'date_registration']].reset_index(drop=True)
+#just_reg['date_registration'] = pd.to_datetime(just_reg['date_registration'], format='%d/%m/%Y')
 
 #catch old registrations that were expanded to include COVID, we can get rid of these for now
 just_reg = just_reg[just_reg['date_registration'] >= pd.Timestamp(2020,1,1)].reset_index(drop=True)
 
-# -
-
-just_reg.head(50)
 
 # +
 just_reg.index = just_reg['date_registration']
@@ -352,9 +340,6 @@ just_reg.index = just_reg['date_registration']
 
 grouped = just_reg.resample('W').count()
 cumsum = grouped.cumsum()
-# -
-
-grouped.head()
 
 # +
 import matplotlib.pyplot as plt
@@ -382,7 +367,7 @@ gr = grouped['trialid'].to_list()
 cs = cumsum['trialid'].to_list()
 
 plt.xticks(x_pos, labels, rotation=45, fontsize=8)
-plt.ylim(-20,800)
+plt.ylim(-50,800)
 plt.xlabel('Week Ending Date')
 plt.ylabel('Registered Trials')
 plt.title('Registered COVID-19 Trials by Week on the ICTRP')
@@ -398,7 +383,7 @@ fig.add_trace(go.Scatter(x=labels, y=grouped['trialid'], fill=None, name='New Tr
 
 fig.add_trace(go.Scatter(x=labels, y=cumsum['trialid'], fill=None, name='Cumulative Trials'))
 
-fig.update_layout(title={'text': 'Registered COVID-19 Trials by Week in 2020', 'xanchor': 'center', 'x': 0.5}, 
+fig.update_layout(title={'text': 'Registered COVID-19 Trials by Week', 'xanchor': 'center', 'x': 0.5}, 
                   xaxis_title='Week Ending Date',
                   yaxis_title='Registered Trials',
                   legend = dict(x=0, y=1, traceorder='normal', bgcolor='rgba(0,0,0,0)'))
@@ -407,7 +392,31 @@ fig.update_layout(title={'text': 'Registered COVID-19 Trials by Week in 2020', '
 
 fig.show()
 #fig.write_html("registered trials.html")
-# -
 
+
+# +
+int_types = df_final.intervention_type.value_counts()
+
+treatment_dict = dict(drugs = int_types['Drug'], 
+                      atmp = int_types['ATMP'], 
+                      clinical_char = int_types['Clinical Presentation'] + int_types['Diagnosis'] + int_types['Prognosis'],
+                      drug_other_combo = int_types['Drug (+ATMP +Other (renal))'] + int_types['Drug (+ATMP)'],
+                      supp = int_types['Supplement'],
+                      geno = int_types['Genomics'],
+                      tm = int_types[[s.startswith('Traditional Medicine') for s in int_types.index]].sum(),
+                      other = int_types[[s.startswith('Other') for s in int_types.index]].sum()
+                     )
+
+fig = go.Figure(go.Bar(
+            x=list(treatment_dict.values()),
+            y=['Drugs', 'ATMP', 'Clinical Characteristics', 'Drug + Other Therapy', 'Supplement', 'Genomics', 'Traditional Medicine', 'Other'],
+            orientation='h'))
+
+fig.update_layout(title={'text': 'Intervention Type of Registered Trials', 'xanchor': 'center', 'x': 0.5}, 
+                  xaxis_title='Number of Trials')
+
+fig.show()
+#fig.write_html('int_bar.html')
+# -
 
 
